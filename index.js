8251ebc013b935f487284319fa38d567faed769b
Tatiana <t.podkur17@gmail.com>
2024-09-16 11:05:29 +0300

    add debounce, input listener, delete button listener

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let REPOSITORIES;
 
 function getRepositories(searchText) {
@@ -13,12 +12,21 @@ function getRepositories(searchText) {
   });
 }
 
+function debounce(fn, ms, callback) {
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args).then(callback), ms);
+  };
+}
+
 function createAutocompleteElement(id, name) {
   let elem = document.createElement('li');
   let button = document.createElement('button');
   button.classList.add('autocomplete__item');
   button.textContent = name;
-  button.dataset.repoId = id;
+  elem.dataset.repoId = id;
   elem.appendChild(button);
   return elem;
 }
@@ -30,15 +38,15 @@ function createRepoInfoElement(text) {
   return nameElem;
 }
 
-function createRepoElement(name, owner, stars) {
+function createRepoElement(repo) {
   let elem = document.createElement('li');
-  elem.classList.add('repo-list__item');
   elem.classList.add('repo');
+  elem.dataset.repoId = repo.id;
 
   let div = document.createElement('div');
-  let nameElem = createRepoInfoElement(`Name: ${name}`);
-  let ownerElem = createRepoInfoElement(`Owner: ${owner}`);
-  let starsElem = createRepoInfoElement(`Stars: ${stars}`);
+  let nameElem = createRepoInfoElement(`Name: ${repo.name}`);
+  let ownerElem = createRepoInfoElement(`Owner: ${repo.owner.login}`);
+  let starsElem = createRepoInfoElement(`Stars: ${repo.watchers}`);
   div.appendChild(nameElem);
   div.appendChild(ownerElem);
   div.appendChild(starsElem);
@@ -52,6 +60,13 @@ function createRepoElement(name, owner, stars) {
   return elem;
 }
 
+function isRepositoryInList(listElement, repoElement) {
+  for (let item of listElement.children) {
+    if (item.dataset.repoId === repoElement.dataset.repoId) return true;
+  }
+  return false;
+}
+
 
 
 
@@ -62,20 +77,36 @@ function createRepoElement(name, owner, stars) {
 
 let autocompleteList = document.querySelector('.autocomplete');
 let repoList = document.querySelector('.repo-list');
+let input = document.querySelector('.search__input');
 
-getRepositories('Q').then(repositories => {
+autocompleteList.addEventListener('click', (event) => {
+  let repo = REPOSITORIES.find(elem => {
+    return elem.id == event.target.parentElement.dataset.repoId;
+  });
+
+  let repoElement = createRepoElement(repo);
+  if (!isRepositoryInList(repoList, event.target.parentElement)) {
+    repoList.appendChild(repoElement);
+  }
+});
+
+const getRepositoriesDebounced = debounce(getRepositories, 100, (repositories) => {
+  autocompleteList.innerHTML = '';
+  if (!repositories || repositories.length === 0) return;
   for (let i = 0; i < 5; i++) {
     let listItem = createAutocompleteElement(repositories[i].id, repositories[i].name);
-    console.log(listItem)
     autocompleteList.appendChild(listItem);
   }
 });
 
-autocompleteList.addEventListener('click', (event) => {
-  let repo = REPOSITORIES.find(elem => {
-    return elem.id == event.target.dataset.repoId;
-  });
+input.addEventListener('keyup', (event) => {
+  if (!event.target.value) return;
+  getRepositoriesDebounced(event.target.value);
+});
 
-  let elem = createRepoElement(repo.name, repo.owner.login, repo.watchers);
-  repoList.appendChild(elem);
+repoList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('repo__close-button')) {
+    const listItem = event.target.parentElement;
+    repoList.removeChild(listItem);
+  }
 });
